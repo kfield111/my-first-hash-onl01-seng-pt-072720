0b889a5c60ba6a5b5d604704fab5e90cb778a270
Kelin Field <kfield111@hotmail.com>
2020-08-08 02:25:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,11 +6,9 @@ end
 
 def shipping_manifest
   the_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2
-    "oil paintings" => 3
-  }
-
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3}
 end
 
 def retrieval
@@ -21,7 +19,6 @@ def retrieval
                  }
 
 the_manifest["oil paintings"]
-
 end
 
 def adding
